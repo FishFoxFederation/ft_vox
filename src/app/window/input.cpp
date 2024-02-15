05ddaf0228048e9e7caa9e80253393dc27e9f367
Simon Gaubert <sim.gaubert.sg@gmail.com>
2024-02-15 14:58:00 +0100

    ADD mouse button and position handling

--- src/app/window/input.cpp
+++ src/app/window/input.cpp
@@ -8,6 +8,8 @@ Input::Input(GLFWwindow* glfwWindow):
 	glfwSetWindowUserPointer(m_window, this);
 
 	glfwSetKeyCallback(m_window, keyCallback);
+	glfwSetMouseButtonCallback(m_window, mouseButtonCallback);
+	glfwSetCursorPosCallback(m_window, cursorPosCallback);
 }
 
 Input::~Input()
@@ -32,6 +34,32 @@ int Input::getKeyState(int key)
 	}
 
 	return state;
+}
+
+int Input::getMouseButtonState(int button)
+{
+	int state = GLFW_RELEASE;
+
+	std::lock_guard<std::mutex> lock(m_mouse_button_state_mutex);
+
+	if (!m_mouse_button_state[button].empty())
+	{
+		state = m_mouse_button_state[button].front();
+
+		if (m_mouse_button_state[button].size() > 1)
+		{
+			m_mouse_button_state[button].pop();
+		}
+	}
+
+	return state;
+}
+
+void Input::getCursorPos(double& xpos, double& ypos)
+{
+	std::lock_guard<std::mutex> lock(m_cursor_pos_mutex);
+	xpos = m_cursor_x;
+	ypos = m_cursor_y;
 }
 
 void Input::keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -61,4 +89,23 @@ void Input::keyCallback(GLFWwindow* window, int key, int scancode, int action, i
 			break;
 		}
 	}
+}
+
+void Input::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+	(void)mods;
+
+	Input* input = static_cast<Input*>(glfwGetWindowUserPointer(window));
+
+	std::lock_guard<std::mutex> lock(input->m_mouse_button_state_mutex);
+	input->m_mouse_button_state[button].push(action);
+}
+
+void Input::cursorPosCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	Input* input = static_cast<Input*>(glfwGetWindowUserPointer(window));
+
+	std::lock_guard<std::mutex> lock(input->m_cursor_pos_mutex);
+	input->m_cursor_x = xpos;
+	input->m_cursor_y = ypos;
 }
