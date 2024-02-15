77647798d084a62489b70f6b77e0893b7579c6a8
afaure42 <afaure42@users.noreply.github.com>
2024-02-15 14:25:43 +0000

    deploy: 05ddaf0228048e9e7caa9e80253393dc27e9f367

--- classInput.js
+++ classInput.js
@@ -1,5 +1,8 @@
 var classInput =
 [
     [ "Input", "classInput.html#aa895b31518ef527a6463c70fb4709097", null ],
-    [ "~Input", "classInput.html#af2db35ba67c8a8ccd23bef6a482fc291", null ]
+    [ "~Input", "classInput.html#af2db35ba67c8a8ccd23bef6a482fc291", null ],
+    [ "getCursorPos", "classInput.html#af27940a5ccc50e41923999fc859033cb", null ],
+    [ "getKeyState", "classInput.html#abf93d666dffe5ebb03ab37e036c2f687", null ],
+    [ "getMouseButtonState", "classInput.html#ae381b13fabd937f971f55f91ad248c58", null ]
 ];
--- search/all_6.js
+++ search/all_6.js
@@ -1,7 +1,10 @@
 var searchData=
 [
-  ['getglfwwindow_0',['getGLFWwindow',['../classWindow.html#aa7ec3835991fe2e00b6a6ab8c3c6d20f',1,'Window']]],
-  ['getmeshrenderdata_1',['getMeshRenderData',['../classWorldScene.html#a31291b76be40ac58d2a7fbebdf7963e8',1,'WorldScene']]],
-  ['getprojectionmatrix_2',['getProjectionMatrix',['../classWorldScene_1_1Camera.html#ae42fd7538d38ad77fac78d260427a8ff',1,'WorldScene::Camera']]],
-  ['getviewmatrix_3',['getViewMatrix',['../classWorldScene_1_1Camera.html#a6f78c8251d703dcd1b4d9cbd81e112d9',1,'WorldScene::Camera']]]
+  ['getcursorpos_0',['getCursorPos',['../classInput.html#af27940a5ccc50e41923999fc859033cb',1,'Input']]],
+  ['getglfwwindow_1',['getGLFWwindow',['../classWindow.html#aa7ec3835991fe2e00b6a6ab8c3c6d20f',1,'Window']]],
+  ['getkeystate_2',['getKeyState',['../classInput.html#abf93d666dffe5ebb03ab37e036c2f687',1,'Input']]],
+  ['getmeshrenderdata_3',['getMeshRenderData',['../classWorldScene.html#a31291b76be40ac58d2a7fbebdf7963e8',1,'WorldScene']]],
+  ['getmousebuttonstate_4',['getMouseButtonState',['../classInput.html#ae381b13fabd937f971f55f91ad248c58',1,'Input']]],
+  ['getprojectionmatrix_5',['getProjectionMatrix',['../classWorldScene_1_1Camera.html#ae42fd7538d38ad77fac78d260427a8ff',1,'WorldScene::Camera']]],
+  ['getviewmatrix_6',['getViewMatrix',['../classWorldScene_1_1Camera.html#a6f78c8251d703dcd1b4d9cbd81e112d9',1,'WorldScene::Camera']]]
 ];
