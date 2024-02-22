dd3dc48b24bec7fa1d7c9228203849c4145d9712
afaure <afaure@student.42.fr>
2024-02-22 20:31:35 +0100

    ADD Perlin classes WIP

---
+++ src/app/perlin/Perlin.hpp
@@ -0,0 +1,26 @@
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
+class Perlin
+{
+public:
+
+private:
+	int octaves = 1;
+	int frequency = 1;
+	
+};
+
+class Perlin2D : public Perlin
+{
+public:
+
+private:
+};
+
+class Perlin3D : public Perlin
+{
+public:
+
+private:
+};
