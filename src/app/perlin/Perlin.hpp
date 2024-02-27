4b11246f9031c322ef67470ea841b9b45ca20b6b
afaure <afaure@student.42.fr>
2024-02-27 14:08:51 +0100

    a

--- src/app/perlin/Perlin.hpp
+++ src/app/perlin/Perlin.hpp
@@ -1,26 +1,32 @@
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
+#include <glm/common.hpp>
+#include <glm/geometric.hpp>
+
 
 class Perlin
 {
 public:
+	Perlin(unsigned int seed);
+	Perlin(unsigned int seed, int octaves, int frequency, float persistence, float lacunarity);
 
+	float			noise(float x) const;
+	float			noise(glm::vec2 v) const;
+	float			noise(glm::vec3 v) const;
 private:
-	int octaves = 1;
-	int frequency = 1;
-	
-};
+	unsigned int	_seed;
+	int 			_octaves = 1;
+	int				_frequency = 1;
+	float			_persistence = 0.5;
+	float			_lacunarity = 2.0;
 
-class Perlin2D : public Perlin
-{
-public:
 
-private:
-};
+	float 			insideNoise(glm::vec3 v, unsigned int seed) const;
+	float			interpolate(float v1, float v2, float v3, float v4, float v5, float v6, float v7, float v8, glm::vec3 t) const;
 
-class Perlin3D : public Perlin
-{
-public:
+	unsigned int	hash(unsigned int x, unsigned int seed) const;
+	unsigned int 	hash(glm::uvec3 v, unsigned int seed) const;
 
-private:
+	glm::vec3		getHashedGradient(glm::uvec3 v, unsigned int seed) const;
+	glm::vec3		getAngleGradient(glm::uvec3 v, unsigned int seed) const;
 };
