4c8ce6536c5209085c4e0ee780081746d22c710b
afaure <afaure@student.42.fr>
2024-03-01 17:52:18 +0100

    ADD Chunk and Block classes

---
+++ src/app/world/Chunk.cpp
@@ -0,0 +1,41 @@
+#include "Chunk.hpp"
+
+Chunk::Chunk(glm::ivec3 position)
+: m_position(position)
+{
+	for(auto & block : m_blocks)
+		block = Block::Air;
+}
+
+Chunk::~Chunk()
+{
+
+}
+
+Block Chunk::getBlock(const int & x, const int & y, const int & z) const
+{
+	int index = toIndex(x, y, z);
+
+	return m_blocks[index];
+}
+
+void Chunk::setBlock(const int & x, const int & y, const int & z, Block block)
+{
+	int index = toIndex(x, y, z);
+
+	m_blocks[index] = block;
+	//REGENERATE MESH HERE
+}
+
+int Chunk::toIndex(const int & x, const int & y, const int & z)
+{
+	return x + y * CHUNK_SIZE + z * CHUNK_SIZE * CHUNK_SIZE;
+}
+
+glm::ivec3	Chunk::toCoord(const int & index)
+{
+	int x = index % CHUNK_SIZE;
+	int y = (index / CHUNK_SIZE) % CHUNK_SIZE;
+	int z = index / (CHUNK_SIZE * CHUNK_SIZE);
+	return glm::ivec3(x, y, z);
+}
