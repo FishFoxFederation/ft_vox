8df3e9b2f24a4a55af755cc434b41c94b3a1d1ee
afaure <afaure@student.42.fr>
2024-03-01 17:51:26 +0100

    ADD hash overload for glm::ivec3

---
+++ src/utils/hashes.hpp
@@ -0,0 +1,16 @@
+#pragma once 
+
+#include <glm/vec3.hpp>
+#include <unordered_map>
+
+namespace std
+{
+	template<>
+	struct hash<glm::ivec3>
+	{
+		std::size_t operator()(const glm::ivec3 & k) const
+		{
+			return std::hash<int>()(k.x) ^ std::hash<int>()(k.y) ^ std::hash<int>()(k.z);
+		}
+	};
+}
