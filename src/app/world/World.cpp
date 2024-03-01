a952fc28a8c290cf8e5bfda2f5faebb25d87d3cf
afaure <afaure@student.42.fr>
2024-03-01 17:52:32 +0100

    ADD class World and WorldGenerator

---
+++ src/app/world/World.cpp
@@ -0,0 +1,26 @@
+#include "World.hpp"
+
+World::World()
+{
+	//generate chunks 4 distance from the player
+	for(int x = -2; x < 2; x++)
+	{
+		for(int y = -2; y < 2; y++)
+		{
+			for(int z = -2; z < 2; z++)
+			{
+				m_chunks.insert(std::make_pair(glm::ivec3(x, y, z), m_worldGenerator.generateChunk(x, y, z)));
+				m_visible_chunks.insert(glm::ivec3(x, y, z));
+			}
+		}
+	}
+}
+
+World::~World()
+{
+}
+
+void World::update(glm::vec3 playerPosition)
+{
+	(void)playerPosition;
+}
