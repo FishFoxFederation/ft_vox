936c370ae742fa0298adc8fb7151bfc066966a37
Simon <sim.gaubert.sg@gmail.com>
2024-03-05 11:27:26 +0100

    IMPROVE shader build:
    - download glslc with a script
    - refactor shader compilation

--- scripts/compile_shaders.sh
+++ scripts/compile_shaders.sh
@@ -1,13 +1,15 @@
 # https://github.com/google/shaderc/blob/main/downloads.md to download glslc
 
-# /usr/local/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-# /usr/local/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
+shader_files=$(find shaders -type f -name "*.vert" -o -name "*.frag")
 
-# ./glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-# ./glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
+for file in $shader_files
+do
+	./external/glslc/glslc $file -o $file.spv
+	if [ $? -ne 0 ]; then
+		echo "Failed to compile $file"
+		exit 1
+	fi
+done
 
-./external/glslc/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-./external/glslc/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
-
-# glslang -V shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-# glslang -V shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
+# ./external/glslc/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+# ./external/glslc/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
