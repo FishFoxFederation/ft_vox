8c70a16ae06fb7c4ccc7b545fec76a767ec629a8
afaure <afaure@student.42.fr>
2024-04-30 14:34:46 +0200

    Moved map inside world class

--- src/app/world/World.hpp
+++ src/app/world/World.hpp
@@ -3,8 +3,13 @@
 #include "define.hpp"
 
 #include "List.hpp"
-#include "Map.hpp"
 #include "Entity.hpp"
+#include "WorldScene.hpp"
+#include "WorldGenerator.hpp"
+#include "logger.hpp"
+#include "CreateMeshData.hpp"
+#include "ThreadPool.hpp"
+#include "VulkanAPI.hpp"
 
 #include <unordered_map>
 
@@ -25,16 +30,107 @@ public:
 	World & operator=(World & other) = delete;
 	World & operator=(World && other) = delete;
 
-	void updateBlock(glm::dvec3 position);
+	
+	
+
 	void updateEntities();
+	void updateBlock(glm::dvec3 position);
 
 private:
-
-	Map m_map;
+	WorldScene &							m_worldScene;
+	VulkanAPI &								m_vulkanAPI;
+	ThreadPool &							m_threadPool;
 
 	IdList<uint64_t, std::shared_ptr<Entity>> m_entities;
 
 	std::shared_ptr<Entity> m_player;
+
+	/*************************************
+	 *  CHUNKS AND MAP
+	*************************************/
+	WorldGenerator							m_worldGenerator;
+	std::unordered_map<glm::ivec3, Chunk>	m_chunks;
+	std::unordered_set<glm::ivec2>			m_loaded_chunks;
+	std::mutex								m_chunks_mutex;
+
+	std::unordered_set<glm::ivec2>			m_visible_chunks;
+	std::mutex								m_visible_chunks_mutex;
+
+	std::unordered_set<glm::ivec2> 			m_unload_set;
+	std::mutex								m_unload_set_mutex;
+
+
+	std::unordered_map<uint64_t, std::future<void>> m_futures;
+
+	std::queue<uint64_t>					m_finished_futures;
+	std::mutex								m_finished_futures_mutex;
+
+	uint64_t								m_future_id = 0;
+
+
+
+	/*********************************************************
+	 * 
+	 *  METHODS
+	 * 
+	*********************************************************/
+
+
+	/*************************************
+	 *  CHUNKS AND MAP
+	*************************************/
+	/**
+	 * @brief will load all chunks around the player
+	 * @warning you must lock m_chunks_mutex before calling this function
+	 * 
+	 * @param playerPosition 
+	 */
+	void 	loadChunks(const glm::vec3 & playerPosition);
+
+	/**
+	 * @brief will load all chunks around the players
+	 * @warning you must lock m_chunks_mutex before calling this function
+	 * 
+	 * @param playerPositions 
+	 */
+	void 	loadChunks(const std::vector<glm::vec3> & playerPositions);
+
+	/**
+	 * @brief will unload chunk that are too far from the player
+	 * @warning you must lock m_chunks_mutex and unload_set_mutex before calling this function
+	 * @param playerPosition 
+	 */
+	void	unloadChunks(const glm::vec3 & playerPosition);
+
+	/**
+	 * @brief will unload chunk that are too far from the players
+	 * @warning you must lock m_chunks_mutex and unload_set_mutex before calling this function
+	 * 
+	 * @param playerPositions 
+	 */
+	void	unloadChunks(const std::vector<glm::vec3> & playerPositions);
+
+	/**
+	 * @brief will mesh chunks that are meshable around the player
+	 * @warning you must lock m_chunks_mutex as well as m_visible_chunks_mutex
+	 * before calling this function
+	 * 
+	 * @param playerPosition 
+	 */
+	void	meshChunks(const glm::vec3 & playerPosition);
+
+	/**
+	 * @brief Will load, unload and mesh chunks around the player
+	 * 
+	 * @param playerPosition 
+	 */
+	void 	updateChunks(const glm::vec3 & playerPosition);
+
+
+	/*************************************
+	 *  FUTURES
+	 *************************************/
+	void	waitForFutures();
 
 };
 
