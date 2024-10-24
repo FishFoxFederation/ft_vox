ae9197002666ed5b655961821a86a3ea25fb6f1e
afaure <afaure@student.42.fr>
2024-10-24 12:00:39 +0200

    added more tests to the task_manager test main

--- src/app/threads/task_manager/main_test.cpp
+++ src/app/threads/task_manager/main_test.cpp
@@ -3,29 +3,66 @@
 
 // compile with 
 // c++ main_test.cpp Executor.cpp ExecutorWorker.cpp -std=c++20 -pthread -I . -I ../pool/ -I ./internal -g3
+using namespace task;
+
+struct test
+{
+	test()
+	: graph(TaskGraph::create()), sub_graph(TaskGraph::create()), sub_graph2(TaskGraph::create())
+	{
+		Task task = graph->emplace(sub_graph);
+		Task task2 = graph->emplace(sub_graph2);
+
+		task2.precede(task);
+
+		sub_graph2->emplace([]() { std::cout << "2" << std::endl; });
+		sub_graph2->emplace([]() { std::cout << "2" << std::endl; });
+		sub_graph2->emplace([]() { std::cout << "2" << std::endl; });
+		sub_graph2->emplace([]() { std::cout << "2" << std::endl; });
+		sub_graph2->emplace([]() { std::cout << "2" << std::endl; });
+
+		sub_graph->emplace([]() { std::cout << "1" << std::endl; });
+		sub_graph->emplace([]() { std::cout << "1" << std::endl; });
+		sub_graph->emplace([]() { std::cout << "1" << std::endl; });
+		sub_graph->emplace([]() { std::cout << "1" << std::endl; });
+		sub_graph->emplace([]() { std::cout << "1" << std::endl; });
+	};
+	std::shared_ptr<TaskGraph> graph;
+	std::shared_ptr<TaskGraph> sub_graph;
+	std::shared_ptr<TaskGraph> sub_graph2;
+};
+
+
+std::shared_ptr<TaskGraph> getGraph()
+{
+	test t;
+
+	return t.graph;
+}
+
 int main()
 {
 	task::Executor executor(4);
-	task::TaskGraph graph;
-	task::TaskGraph sub_graph;
-	task::TaskGraph sub_graph2;
-	task::TaskGraph sub_sub_graph;
+	std::shared_ptr<task::TaskGraph> graph = TaskGraph::create();
+	std::shared_ptr<task::TaskGraph> sub_graph = TaskGraph::create();
+	std::shared_ptr<task::TaskGraph> sub_graph2 = TaskGraph::create();
+	std::shared_ptr<task::TaskGraph> sub_sub_graph = TaskGraph::create();
 
-	task::Task A = graph.emplace([]() { std::cout << "A" << std::endl; });
-	task::Task B = graph.emplace([]() { std::cout << "B" << std::endl; });
-	task::Task C = graph.emplace([]() { std::cout << "C" << std::endl; }); 
-	task::Task D = graph.emplace([]() { std::cout << "D" << std::endl; });
-
-
-
-	task::Task submodule = graph.emplace(sub_graph);
-	task::Task submodule2 		= sub_graph.emplace(sub_graph2);
-	task::Task sub_sub_module   = sub_graph2.emplace(sub_sub_graph);
+	task::Task A = graph->emplace([]() { std::cout << "A" << std::endl; });
+	task::Task B = graph->emplace([]() { std::cout << "B" << std::endl; });
+	task::Task C = graph->emplace([]() { std::cout << "C" << std::endl; }); 
+	task::Task D = graph->emplace([]() { std::cout << "D" << std::endl; });
 
 
-	task::Task sub_task		= sub_graph.emplace([]() { std::cout << "1" << std::endl; }).Name("1");
-	task::Task sub_task2	= sub_graph2.emplace([]() { std::cout << "2" << std::endl; }).Name("2");
-	task::Task sub_sub_task	= sub_sub_graph.emplace([]() { std::cout << "3" << std::endl; }).Name("3");
+
+	task::Task submodule = graph->emplace(sub_graph);
+	task::Task submodule2 		= sub_graph->emplace(sub_graph2);
+	task::Task sub_sub_module   = sub_graph2->emplace(sub_sub_graph);
+
+
+	task::Task sub_task		= sub_graph->emplace([]() { std::cout << "1" << std::endl; }).Name("1");
+	task::Task sub_task2	= sub_graph2->emplace([]() { std::cout << "2" << std::endl; }).Name("2");
+	task::Task sub_sub_task	= sub_sub_graph->emplace([]() { std::cout << "3" << std::endl; }).Name("3");
 
 	A.Name("A");
 	B.Name("B");
@@ -47,5 +84,11 @@ int main()
 	sub_sub_module.precede(sub_task2);
 
 	executor.run(graph);
+	executor.waitForAll();
+
+
+
+	std::shared_ptr<task::TaskGraph> graph2 = getGraph();
+	executor.run(graph2);
 	executor.waitForAll();
 };
