661fe1705f08868847bfe3a4cd4f81b82d807a54
afaure <afaure@student.42.fr>
2024-11-07 18:22:05 +0100

    add tracy build script

--- do-wonders.sh
+++ do-wonders.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
+ROOT_DIR=$(pwd)
+TRACY_MULTICAPTURE_DIR=$ROOT_DIR/external/tracy-experimental/capture
+TRACY_MERGE_DIR=$ROOT_DIR/external/tracy-experimental/multiprocess
+TRACY_PROFILER_DIR=$ROOT_DIR/external/tracy/profiler
+
+
+if [ ! -d ./tracy ]
+then
+	echo "building"
+	mkdir -p tracy
+
+	# build tracy-multicapture
+	cd $TRACY_MULTICAPTURE_DIR
+	cmake -B build
+	cmake --build build
+	mv build/tracy-multicapture $ROOT_DIR/tracy
+
+	# build tracy-merge
+	cd $TRACY_MERGE_DIR
+	cmake -B build
+	cmake --build build
+	mv build/tracy-merge $ROOT_DIR/tracy
+
+	# build tracy-profiler
+	cd $TRACY_PROFILER_DIR
+	cmake -B build
+	cmake --build build
+	mv build/tracy-profiler $ROOT_DIR/tracy
+fi
+
+cd $ROOT_DIR/tracy
 # launch multicapture
-./tracy-multicapture -o output -M -f &
+tracy-multicapture -o output -M -f &
 
 PID_MULTICAPTURE=$!
 
 # launch server
-./build/server &
+$ROOT_DIR/build/server &
 PID_SERVER=$!
 
 sleep 1
-# launch client
+# launch client ( from root dir)
+cd $ROOT_DIR
 ./build/ft_vox localhost 4245 5 2> /dev/null
+cd $ROOT_DIR/tracy
 
 # now we wait for client to stop exec
 #( we do nothing because its not launched async )
