14ef43156984f2e3fa8763cff4cd24ca2c902366
afaure42 <afaure@student.42.fr>
2024-11-11 13:15:16 +0100

    clean: Client.cpp and Client.hpp

--- src/app/network/client/Client.hpp
+++ src/app/network/client/Client.hpp
@@ -10,6 +10,30 @@
 #include "PacketFactory.hpp"
 #include "ThreadSafePacketQueue.hpp"
 
+/**
+ * @brief The Client side interface with the network module.
+ * 
+ * @details This class is responsible for managing the connection to the server 
+ *  and sending and receiving packets.
+ * 
+ * It is fully threadsafe and can be run in a separate thread.
+ * 
+ * About the sendPacketNoWait overload:
+ * 
+ * If the client is running on another thread this can be usefull.
+ * when this method is called the packet will be queued to be sent and no additionnal
+ * manipulation will be done. All the serialization and syscalls will be done in the client thread.
+ * If the packet being sent isnt highly delay sensitive this method should be used.
+ * It wont change anything regarding performances if the client is running on the same thread as the sending thread.
+ * 
+ * 
+ * About the ServerDisconnected exception:
+ * 
+ * It is the client's way of of notifying the user that the server disconnected.
+ * 
+ * the runOnce method will throw this exception if the server disconnected.
+ * 
+ */
 class Client
 {
 public:
@@ -22,9 +46,8 @@ public:
 	Client& operator=(Client&& other) = delete;
 
 	void runOnce(const int & timeout = -1);
-	void run();
 	void sendPacket(std::shared_ptr<IPacket> packet);
-	void sendPacketNoWait(std::shared_ptr<IPacket> packet);
+	void sendPacketNoWait(std::shared_ptr<IPacket> packet) noexcept;
 
 	class ServerDisconnected : public std::exception
 	{
@@ -42,7 +65,6 @@ public:
 	std::unordered_map<uint64_t, std::chrono::time_point<std::chrono::high_resolution_clock>> m_pings;
 private:
 	
-	bool											m_running;
 	Poller											m_poller;
 	std::shared_ptr<Socket>							m_client_socket;
 	Connection										m_connection;
