7cccbd0c1d98d400f90b82df514adaec0277dc17
afaure42 <afaure@student.42.fr>
2024-11-11 15:28:39 +0100

    add: sendInfo struct model to client class just like server class

--- src/app/network/client/Client.hpp
+++ src/app/network/client/Client.hpp
@@ -18,14 +18,13 @@
  * 
  * It is fully threadsafe and can be run in a separate thread.
  * 
- * About the sendPacketNoWait overload:
+ * About the sendInfo struct and the ASYNC flag:
  * 
- * If the client is running on another thread this can be usefull.
- * when this method is called the packet will be queued to be sent and no additionnal
- * manipulation will be done. All the serialization and syscalls will be done in the client thread.
- * If the packet being sent isnt highly delay sensitive this method should be used.
+ *  if the client is running on another thread this flag can be usefull.
+ *  when this flag is set the send function will only queue the packet to be sent.
+ *  All of the serialization and the syscalls will be done in the client thread.
+ *  If the packet being sent isnt highly delay sensitive this flag should be used.
  * It wont change anything regarding performances if the client is running on the same thread as the sending thread.
- * 
  * 
  * About the ServerDisconnected exception:
  * 
@@ -45,9 +44,19 @@ public:
 	Client(Client&& other) = delete;
 	Client& operator=(Client&& other) = delete;
 
-	void runOnce(const int & timeout = -1);
-	void sendPacket(std::shared_ptr<IPacket> packet);
-	void sendPacketNoWait(std::shared_ptr<IPacket> packet) noexcept;
+	void runOnce(const int & timeout_ms = -1);
+
+	enum flags : uint8_t
+	{
+		ASYNC = 1
+	};
+
+	struct sendInfo
+	{
+		std::shared_ptr<IPacket> packet;
+		uint8_t flags;
+	};
+	void send(const sendInfo & info);
 
 	class ServerDisconnected : public std::exception
 	{
@@ -75,4 +84,6 @@ private:
 	int		readData();
 	int		sendData();
 	void	emptyOutgoingPackets();
+
+	void	sendPacket(std::shared_ptr<IPacket> packet);
 };
