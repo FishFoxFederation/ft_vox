e859f21f81bea3b96d0748fd78f2a6a0b5220324
afaure42 <afaure@student.42.fr>
2024-11-19 21:07:09 +0100

    add: some views methods

--- src/app/world/entity/ECS_v2/View.hpp
+++ src/app/world/entity/ECS_v2/View.hpp
@@ -2,14 +2,33 @@
 
 #include "ECS.hpp"
 
+
+
 namespace ecs
 {
+	template <typename... ComponentTypes>
+	class viewIterator
+	{
+	public:
+		viewIterator();
+		~viewIterator();
+
+		viewIterator & operator++();
+		viewIterator operator++(int);
+		bool operator==(const viewIterator & other) const;
+		bool operator!=(const viewIterator & other) const;
+
+	private:
+
+	};
+
 	template <typename... ComponentTypes>
 	class View
 	{
 	public:
-		View(ECS & ecs)
+		View(ECS & ecs);
 
+		
 	private:
 		ECS & m_ecs;
 		std::tuple<std::shared_ptr<SparseSet<ComponentTypes>>...> m_components;
