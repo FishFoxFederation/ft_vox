fc7955bf55811e1d2b96029a9ec7523901fe30ec
afaure42 <afaure@student.42.fr>
2024-11-20 21:03:21 +0100

    add: entity reuse system + fully working sparseSet + test main

--- src/app/world/entity/ECS_v2/ECS.hpp
+++ src/app/world/entity/ECS_v2/ECS.hpp
@@ -6,51 +6,102 @@
 #include <exception>
 #include <string>
 #include <functional>
+#include <tuple>
+#include <utility>
+#include <iostream>
 
 #include "ECS_CONSTANTS.hpp"
-
+#include "ECS_FWD.hpp"
 
 #include "SparseSet.hpp"
+#include "View.hpp"
 
 namespace ecs
 {
 
+	/**
+	 * @brief 
+	 * 
+	 * @tparam size 
+	 */
 	template <size_t size = MAX_ENTITIES>
 	class ECS
 	{
 	public:
-		ECS();
-		~ECS();
+		ECS()
+		{};
+		~ECS(){};
 
 		ECS(ECS & other) = delete;
 		ECS(ECS && other) = delete;
 		ECS & operator=(ECS & other) = delete;
 		ECS & operator=(ECS && other) = delete;
 
-		entityType		createEntity()
+		/*************************************************************\
+		 * 	ENTITY
+		\*************************************************************/
+		std::pair<bool, entityType>		createEntity()
 		{
+			if (m_entities.size() == MAX_ENTITIES)
+				return {false, 0};
+			//check if there are any dead entities to reuse
+			if (m_dead_entity_head != 0)
+			{
+				entityType dead_entity = m_dead_entity_head;
+				auto dead_index = getEntityIndex(dead_entity);
 
+				//set the new dead entity head to the next dead entity
+				//see doc for more info
+				m_dead_entity_head = m_entities[dead_index];
+
+				IncreaseEntityVersion(dead_entity);
+				m_entities[dead_index] = dead_entity;
+				return {true, dead_entity};
+			}
+			else
+			{
+				auto new_entity = createNewEntity();
+				m_entities.push_back(new_entity);
+				return {true, new_entity};
+			}
 		}
 
 		void			removeEntity(entityType entity)
 		{
+			if (!isAlive(entity))
+				throw EntityDoesNotExist(entity);
+			
+			auto index = getEntityIndex(entity);
+			
+			std::swap(m_entities[index], m_dead_entity_head);
 
+			//todo remove entity from all components
 		}
 
 		bool 			isAlive(entityType entity) const
 		{
+			auto index = getEntityIndex(entity);
+
+			if (index >= m_entities.size())
+				return false;
 			
+			if (m_entities[index] != entity)
+				return false;
+			return true;
 		}
 
+		/*************************************************************\
+		 * 	COMPONENTS
+		\*************************************************************/
 		//add component
 		template <typename ComponentType>
 		void addComponent()
 		{
 			const std::type_index type = std::type_index(typeid(ComponentType));
 			if (m_components.contains(type))
-				throw ComponentAlreadyExists();
+				throw ComponentAlreadyExists<ComponentType>();
 
-			m_components[type] = std::make_shared<SparseSet<ComponentType, size>>();
+			m_components[type] = std::make_shared<SparseSet<ComponentType>>();
 		}
 
 		//remove component
@@ -59,11 +110,15 @@ namespace ecs
 		{
 			const std::type_index type = std::type_index(typeid(ComponentType));
 			if (!m_components.contains(type))
-				throw ComponentDoesNotExist();
+				throw ComponentDoesNotExist<ComponentType>();
 
 			m_components.erase(type);
 		}
 
+		
+		/*************************************************************\
+		 * 	COMPONENT-ENTITY RELATION
+		\*************************************************************/
 		//add component to entity
 		template <typename ComponentType>
 		void addComponentToEntity(entityType entity)
@@ -79,6 +134,14 @@ namespace ecs
 		//get component from entity
 		template <typename ComponentType>
 		ComponentType & getComponentFromEntity(entityType entity)
+		{
+		}
+
+		/*************************************************************\
+		 * 	VIEWS
+		\*************************************************************/
+		template <typename... ComponentTypes>
+		View<ComponentTypes...> view()
 		{
 		}
 
@@ -108,11 +171,12 @@ namespace ecs
 
 		};
 
+		template <typename ComponentType>
 		class ComponentDoesNotExist : public std::exception
 		{
 		public:
-			ComponentDoesNotExist(const std::type_index & type)
-				: m_type(type) {}
+			ComponentDoesNotExist()
+				: m_type(typeid(ComponentType)) {}
 			const char * what() const noexcept override
 			{
 				std::string message = "Component does not exist ";
@@ -124,11 +188,12 @@ namespace ecs
 			std::type_index m_type;
 		};
 
+		template <typename ComponentType>
 		class ComponentAlreadyExists : public std::exception
 		{
 		public:
-			ComponentAlreadyExists(const std::type_index & type)
-				: m_type(type) {}
+			ComponentAlreadyExists()
+				: m_type(typeid(ComponentType)) {}
 			const char * what() const noexcept override
 			{
 				std::string message = "Component already exists ";
@@ -141,10 +206,30 @@ namespace ecs
 		};
 
 	private:
-		std::array<entityType, MAX_ENTITIES> m_entities;
+		std::vector<entityType> m_entities;
+
+		entityType m_dead_entity_head = 0;
 
 		std::unordered_map<std::type_index, std::shared_ptr<void>> m_components;
 
+		uint32_t getEntityVersion(entityType entity) const
+		{
+			return entity & 0xFFF;
+		}
 
+		uint32_t getEntityIndex(entityType entity) const
+		{
+			return entity >> 3;
+		}
+
+		void IncreaseEntityVersion(entityType & entity)
+		{
+			entity += (getEntityVersion(entity) + 1) & 0xFFF;
+		}
+
+		entityType createNewEntity()
+		{
+			return m_entities.size() << 3 | 1;
+		}
 	};
 }
---
+++ src/app/world/entity/ECS_v2/ECS_FWD.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ECS_CONSTANTS.hpp"
+
+namespace ecs
+{
+	// template <typename... ComponentTypes>
+	// class viewIterator;
+
+	// template <typename... ComponentTypes>
+	// class View;
+
+	// template <size_t size = MAX_ENTITIES>
+	// class ECS;
+}
