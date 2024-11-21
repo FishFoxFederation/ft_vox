b89dfd28e33309e9e7c0e1effbc90f740ad1337b
afaure42 <afaure@student.42.fr>
2024-11-21 21:47:34 +0100

    wip ecs2

--- src/app/world/entity/ECS_v2/View.hpp
+++ src/app/world/entity/ECS_v2/View.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <unordered_map>
+#include <array>
+
 #include "Storage.hpp"
+#include "SparseSet.hpp"
 
 namespace ecs
 {
-	template <typename... ComponentTypes>
+	template <typename EntityType, typename... ComponentTypes>
 	class viewIterator
 	{
 	public:
@@ -17,20 +21,39 @@ namespace ecs
 		viewIterator	operator--(int);
 		bool			operator==(const viewIterator & other) const;
 		bool			operator!=(const viewIterator & other) const;
-
+		
 	private:
-
 	};
 
-	template <typename... ComponentTypes>
+	template <typename EntityType, typename... ComponentTypes>
 	class View
 	{
 	public:
-		View();
-		// View(Storage & ecs) {};
+		typedef viewIterator<EntityType, ComponentTypes...> iterator;
+		template <size_t storage_size>
+		View(ecs::Storage<EntityType, storage_size> & storage)
+		{
+			auto func = [&]<typename ComponentType>()
+			{
+				using SparseSetType = SparseSet<EntityType, ComponentType>;
+				auto set = storage.getSetPtr<ComponentType>();
+				m_sparseSets.insert({std::type_index(typeid(ComponentType)), set});
+			};
+			(func.template operator()<ComponentTypes>(), ...);
+		}
 
-		viewIterator<ComponentTypes...>	begin();
-		viewIterator<ComponentTypes...>	end();
+		~View()
+		{			
+		}
+
+		//begin and end
+		iterator begin()
+		{
+			//find the smallest set
+		}
 	private:
+		typedef std::tuple<std::pair<ComponentTypes, size_t> ...> indices_tuple;
+		typedef std::unordered_map<std::type_index, std::shared_ptr<void>> SparseSets_storage;
+		SparseSets_storage m_sparseSets;
 	};
 };
