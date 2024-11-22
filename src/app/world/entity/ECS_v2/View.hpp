68bfa57eae5767ce212fb4bd697094475f9442fa
afaure42 <afaure@student.42.fr>
2024-11-22 16:20:18 +0100

    add: first iteration of view system now functionnal, needs more testing

--- src/app/world/entity/ECS_v2/View.hpp
+++ src/app/world/entity/ECS_v2/View.hpp
@@ -2,6 +2,9 @@
 
 #include <unordered_map>
 #include <array>
+#include <limits>
+
+#include "ecs_FWD.hpp"
 
 #include "Manager.hpp"
 #include "ComponentStorage.hpp"
@@ -13,11 +16,28 @@ namespace ecs
 	class viewIterator
 	{
 	public:
-		viewIterator();
-		~viewIterator();
+		typedef SparseSet<EntityType> entitySet;
+		typedef entitySet::iterator entityIterator;
+		typedef std::shared_ptr<entitySet> SparseSetPtr;
+		typedef std::array<SparseSetPtr, pool_size> SparseSetArray;
+
+		viewIterator(std::unordered_map<std::type_index, SparseSetPtr> & sparseSets, SparseSetPtr leadSet, entityIterator leadIter = entityIterator())
+			: m_sparseSets{}, m_leadSet(leadSet), m_lead_iterator(leadIter)
+		{
+			size_t i = 0;
+			for(auto & [type, set] : sparseSets)
+			{
+				m_sparseSets[i] = set;
+				i++;
+			}
+			seek_next();
+		}
+
+		~viewIterator(){};
 
 		viewIterator &	operator++()
 		{
+			m_lead_iterator++;
 			seek_next();
 			return *this;
 		}
@@ -25,12 +45,13 @@ namespace ecs
 		viewIterator	operator++(int)
 		{
 			viewIterator temp = *this;
-			seek_next();
+			++(*this);
 			return temp;
 		}
 
 		viewIterator &	operator--()
 		{
+			--m_lead_iterator;
 			seek_prev();
 			return *this;
 		}
@@ -38,13 +59,13 @@ namespace ecs
 		viewIterator	operator--(int)
 		{
 			viewIterator temp = *this;
-			seek_prev();
+			--(*this);
 			return temp;
 		}
 
 		bool			operator==(const viewIterator & other) const
 		{
-			return (*m_lead_iterator == *other.m_lead_iterator);
+			return (m_lead_iterator == other.m_lead_iterator);
 		}
 
 		bool			operator!=(const viewIterator & other) const
@@ -58,10 +79,7 @@ namespace ecs
 		}
 		
 	private:
-		typedef SparseSet<EntityType> entitySet;
-		typedef entitySet::iterator entityIterator;
-		typedef std::shared_ptr<entitySet> SparseSetPtr;
-		typedef std::array<SparseSetPtr, pool_size> SparseSetArray;
+		
 
 		bool all_of(EntityType entity)
 		{
@@ -99,19 +117,32 @@ namespace ecs
 		typedef SparseSet<EntityType> entitySet;
 		typedef viewIterator<EntityType, sizeof... (ComponentTypes)> iterator;
 
-		// template <size_t storage_size>
-		// View(ecs::Manager<EntityType, storage_size> & storage)
-		// {
-		// 	auto func = [&]<typename ComponentType>()
-		// 	{
-		// 		using ComponentSet = ComponentStorage<EntityType, ComponentType>;
-		// 		std::shared_ptr<ComponentSet> set = storage.getSetPtr<ComponentType>();
+		template <size_t manager_size>
+		View( Manager<EntityType, manager_size> & storage)
+		{
+			//mmmmm spicy
+			auto func = [&]<typename ComponentType>()
+			{
+				using ComponentSet = ComponentStorage<EntityType, ComponentType>;
+				std::shared_ptr<ComponentSet> set = storage.template getSetPtr<ComponentType>();
 
-		// 		m_sparseSets.insert({std::type_index(typeid(ComponentType)), set});
-		// 	};
-		// 	(func.template operator()<ComponentTypes>(), ...);
-		// }
+				m_sparseSets.insert({std::type_index(typeid(ComponentType)), set});
+			};
+			(func.template operator()<ComponentTypes>(), ...);
+
+			size_t min = std::numeric_limits<size_t>::max();
+			for(auto & [type, set] : m_sparseSets)
+			{
+				if (set->size() < min)
+				{
+					min = set->size();
+					m_lead = type;
+				}
+			}
+		}
+
 		View(){};
+
 		~View()
 		{			
 		}
@@ -136,9 +167,16 @@ namespace ecs
 		iterator begin()
 		{
 			//give the iterator a lead set and the rest of the sets
+			return iterator(m_sparseSets, m_sparseSets[m_lead], m_sparseSets[m_lead]->begin());
+		}
+
+		iterator end()
+		{
+			return iterator(m_sparseSets, m_sparseSets[m_lead], m_sparseSets[m_lead]->end());
 		}
 	private:
 		std::unordered_map<std::type_index, std::shared_ptr<entitySet>> m_sparseSets;
+		std::type_index m_lead = typeid(void);
 	};
 
 };
