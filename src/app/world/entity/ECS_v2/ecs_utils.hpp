00c90ba10657329880d70029b68857a5a1405b21
afaure42 <afaure@student.42.fr>
2024-11-29 12:08:31 +0100

    change: modified view class to take an array of sparse sets instead of a map

--- src/app/world/entity/ECS_v2/ecs_utils.hpp
+++ src/app/world/entity/ECS_v2/ecs_utils.hpp
@@ -10,4 +10,23 @@ namespace ecs
 	template <typename I>
 	concept ValidEntity = std::integral<I> && std::unsigned_integral<I>;
 
+	template <typename T, typename... Ts>
+	concept is_any = std::disjunction_v<std::is_same<T, Ts>...>;
+
+	template <typename... Args>
+	struct IndexOf
+	{
+		template <typename T> requires is_any<T, Args...>
+		static size_t get()
+		{
+			static size_t local_index = index++;
+
+			return local_index;
+		}
+	private:
+		static size_t index;
+	};
+
+	template <typename... Args>
+	size_t IndexOf<Args...>::index = 0;
 }
